a8947baf3f194fc281298abe9662f3747853e64d
neha wagadre <neha.wagadre19@gmail.com>
2021-12-19 21:27:24 +0530

    message commit

--- src/main/java/com/example/Cnatives/CnativesApplication.java
+++ src/main/java/com/example/Cnatives/CnativesApplication.java
@@ -2,10 +2,17 @@ package com.example.Cnatives;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class CnativesApplication {
 
+	@GetMapping("/message")
+	public String message() {
+		return "azure";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CnativesApplication.class, args);
 	}
